67832d34b205cd1c414981b2ba313944b98ecb4d
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-04-08 07:12:12 +0300

    Add contact page

--- Dentist/website/urls.py
+++ Dentist/website/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page_view, name="home")
+    path('', views.home_page_view, name="home"),
+    path('contacts/', views.contact_page_view, name="contacts"),
 ]
--- Dentist/website/views.py
+++ Dentist/website/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def home_page_view(request):
     return render(request, 'index.html', {})
+
+
+def contact_page_view(request):
+    return render(request, 'contacts.html', {})
